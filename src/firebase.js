e4a1a7808b3788bac9c0f0b05324a2da4b0f0eaa
Tahirkhanfwork <tahirkhanfwork@gmail.com>
2025-10-08 17:22:45 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
-process.env.TZ = 'UTC';
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
